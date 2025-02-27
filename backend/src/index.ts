7668fe16a3a7e41ca6b1c40bec3a22a9ca7373df
rahul <rahul.rajeevan4@gmail.com>
2025-02-27 16:35:26 +0530

    Fixed PORT type issue and updated app.listen

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -41,7 +41,10 @@ app.use("/",(req, res) => {
 }
 );
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+const PORT = Number(process.env.PORT) || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
